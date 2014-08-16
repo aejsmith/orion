652a37819ab73909b504fdc1cb3d1999a1eba4dd
Alex Smith <alex@alex-smith.me.uk>
2014-08-16 16:20:42 +0100

    Update sample file for future shader format
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- engine/assets/shaders/test.glsl
+++ engine/assets/shaders/test.glsl
@@ -10,8 +10,8 @@
  * stage.
  *
  * We do kind of duplicate the layout qualifier functionality provided by GL
- * here, but our target GL version is 4.1 (the latest supported by OS X) and
- * this does not support layout qualifiers for uniform blocks and samplers.
+ * here, but our target GL version is 3.3 and this does not support layout
+ * qualifiers for uniform blocks and samplers.
  *
  * With the resource system, a GPUProgram is a single resource. Since we can
  * have multiple GPUPrograms generated from the same source file, we will key
@@ -19,7 +19,7 @@
  */
 
 /* Target GLSL version, copied into each generated shader. */
-@version 410
+@version 330
 
 /* Vertex attribute definitions, bound based on semantic and optional index. */
 @attribute vec3 attrib_position : kPositionSemantic
@@ -27,22 +27,21 @@
 @attribute vec2 attrib_texcoord : kTexCoordSemantic[0]
 
 /* Custom uniform block type definition. */
-@typedef MaterialParams {
+@typedef MaterialUniforms {
 	float shininess;
 	vec3 specular;
 }
 
 /*
  * Uniform block definitions, with type and an optional instance name. There are
- * a number of predefined types such as per-light and per-object parameters. The
+ * a number of predefined types such as per-light and per-entity parameters. The
  * final field specifies the block binding point, with an optional index (e.g.
  * for the custom block binding range).
  */
-@uniforms LightParams light : kCurrentLightParams
-@uniforms ObjectParams object : kCurrentObjectParams
-@uniforms CameraParams camera : kCameraParams
-@uniforms WorldParams world : kWorldParams
-@uniforms MaterialParams material : kCustomParams[0]
+@uniforms LightUniforms light : kLightUniforms
+@uniforms EntityUniforms entity : kEntityUniforms
+@uniforms ViewUniforms view : kViewUniforms
+@uniforms MaterialUniforms material : kCustomUniforms[0]
 
 /*
  * Sampler definition. The final field specifies the texture unit. There are
@@ -70,11 +69,11 @@
 @out vec2 vtx_texcoord;
 
 void main() {
-	vtx_position = vec3(object.transform * vec4(attrib_position, 1.0));
-	vtx_normal = vec3(object.transform * vec4(attrib_normal, 0.0));
+	vtx_position = vec3(entity.transform * vec4(attrib_position, 1.0));
+	vtx_normal = vec3(entity.transform * vec4(attrib_normal, 0.0));
 	vtx_texcoord = attrib_texcoord;
 
-	gl_Position = object.transform * camera.vp_matrix * vec4(attrib_position, 1.0);
+	gl_Position = view.view_projection * entity.transform * vec4(attrib_position, 1.0);
 }
 
 /*
@@ -84,7 +83,8 @@ void main() {
 @program kFragmentProgram
 @out vec4 frag_colour : 0
 
-/* Calculate the lighting contribution of a light. */
+/** Calculate the lighting contribution of a light.
+ * @param direction	Direction from the light to the fragment. */
 vec4 calc_light(vec3 direction) {
 	vec4 total_factor = vec4(0.0);
 	vec3 normal = normalize(vtx_normal);
@@ -99,26 +99,32 @@ vec4 calc_light(vec3 direction) {
 
 		/* Do specular reflection using Blinn-Phong. Calculate the
 		 * cosine of the angle between the normal and the half vector. */
-		vec3 half_vector = normalize(direction - (vtx_position - camera.position));
+		vec3 half_vector = normalize(direction - (vtx_position - view.position));
 		float specular_angle = dot(normal, half_vector);
 		if(specular_angle > 0.0) {
-			total_factor += vec4(light.colour, 1.0)
-				* pow(specular_angle, material.shininess)
-				* vec4(material.specular, 1.0);
+			total_factor +=
+				vec4(light.colour, 1.0) *
+				pow(specular_angle, material.shininess) *
+				vec4(material.specular, 1.0);
 		}
 	}
 
 	return total_factor;
 }
 
-/* Calculate the lighting contribution of a directional light. */
+/** Calculate the lighting contribution of an ambient light. */
+vec4 ambient_light_factor() {
+	return vec4(light.colour * light.intensity, 1.0);
+}
+
+/** Calculate the lighting contribution of a directional light. */
 vec4 directional_light_factor() {
 	return calc_light(light.direction);
 }
 
-/* Calculate the lighting contribution of a point light. */
+/** Calculate the lighting contribution of a point light. */
 vec4 point_light_factor() {
-	/* Calculate distance to light and direction to the vertex. */
+	/* Calculate distance to light and direction to the fragment. */
 	vec3 light_to_vertex = vtx_position - light.position;
 	float distance = length(light_to_vertex);
 	vec3 direction = normalize(light_to_vertex);
@@ -131,38 +137,45 @@ vec4 point_light_factor() {
 	vec4 light_factor = calc_light(direction);
 
 	/* Apply attenuation. */
-	float attenuation = light.constant + (light.linear * distance) + (light.exp * distance * distance);
+	float attenuation =
+		light.attenuation_constant +
+		(light.attenuation_linear * distance) +
+		(light.attenuation_exp * distance * distance);
+
 	return light_factor / attenuation;
 }
 
-/* Calculate the lighting contribution of a spot light. */
+/** Calculate the lighting contribution of a spot light. */
 vec4 spot_light_factor() {
 	vec3 light_to_vertex = normalize(vtx_position - light.position);
 	float spot_factor = dot(light_to_vertex, light.direction);
 
-	if(spot_factor > light_cutoff) {
+	if(spot_factor > light.cos_cutoff) {
 		/* Same as point light calculation, with cone edge softened. */
-		float light_factor = point_light_factor();
-		return light_factor * (1.0 - (1.0 - spot_factor) * (1.0 / (1.0 - light.cutoff)));
+		return point_light_factor() * (1.0 - (1.0 - spot_factor) * (1.0 / (1.0 - light.cos_cutoff)));
 	} else {
 		return vec4(0.0);
 	}
 }
 
 void main() {
-	vec4 texel = texture2D(texture, vtx_texcoord);
-
 	/* Determine the light factor for our light source. */
 	vec4 light_factor = vec4(0.0);
-	if(light.intensity > 0.0) {
-		if(light_type == kDirectionalLight) {
-			light_factor = directional_light_factor();
-		} else if(light_type == kPointLight) {
-			light_factor = point_light_factor();
-		} else {
-			light_factor = spot_light_factor();
-		}
+	switch(light.type) {
+	case kAmbientLight:
+		light_factor = ambient_light_factor();
+		break;
+	case kDirectionalLight:
+		light_factor = directional_light_factor();
+		break;
+	case kPointLight:
+		light_factor = point_light_factor();
+		break;
+	case kSpotLight:
+		light_factor = spot_light_factor();
+		break;
 	}
 
-	frag_colour = texel * (vec4(world.ambient_colour, 1.0) + light_factor);
+	vec4 texel = texture2D(texture, vtx_texcoord);
+	frag_colour = texel * light_factor;
 }
