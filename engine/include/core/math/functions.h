f4e0cea6b16e76bb213be26d117cf47cb4e2d89e
Alex Smith <alex@alex-smith.me.uk>
2014-09-11 10:12:59 +0100

    Make math functions constexpr
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- engine/include/core/math/functions.h
+++ engine/include/core/math/functions.h
@@ -15,7 +15,7 @@ namespace math {
  * @param nearest	Boundary to round up to.
  * @return		Rounded value. */
 template <typename T, typename U>
-inline T roundUp(T val, const U &nearest) {
+inline constexpr T roundUp(const T &val, const U &nearest) {
 	/* When nearest is a power of 2, this is optimised to be equivalent
 	 * to the following:
 	 *  if(val & (nearest - 1)) {
@@ -24,12 +24,9 @@ inline T roundUp(T val, const U &nearest) {
 	 *  }
 	 * Using the implementation below has the benefit that we do not limit
 	 * use to power-of-2 alignment. */
-	if(val % nearest) {
-		val -= val % nearest;
-		val += nearest;
-	}
-
-	return val;
+	return (val % nearest)
+		? (val - (val % nearest)) + nearest
+		: val;
 }
 
 /** Round a value down.
@@ -37,19 +34,18 @@ inline T roundUp(T val, const U &nearest) {
  * @param nearest	Boundary to round down to.
  * @return		Rounded value. */
 template <typename T, typename U>
-inline T roundDown(T val, const U &nearest) {
+inline constexpr T roundDown(const T &val, const U &nearest) {
 	/* Same as above. */
-	if(val % nearest)
-		val -= val % nearest;
-
-	return val;
+	return (val % nearest)
+		? val - (val % nearest)
+		: val;
 }
 
 /** Check if a value is a power of 2.
  * @param val		Value to check.
  * @return		Whether value is a power of 2. */
 template <typename T>
-inline bool isPow2(T val) {
+inline constexpr bool isPow2(const T &val) {
 	return val && (val & (val - 1)) == 0;
 }
 
