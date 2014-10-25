5e0c9e23e097c915eb56e87105b478d7d142856e
Alex Smith <alex@alex-smith.me.uk>
2014-10-25 14:08:06 +0100

    Allow specification of address mode in texture metadata
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

---
+++ engine/src/engine/loaders/texture_loader.h
@@ -0,0 +1,31 @@
+/**
+ * @file
+ * @copyright		2014 Alex Smith
+ * @brief		Texture loader classes.
+ */
+
+#pragma once
+
+#include "engine/asset_loader.h"
+#include "engine/texture.h"
+
+/** 2D texture loader base class. */
+class Texture2DLoader : public AssetLoader {
+public:
+	AssetPtr load() override;
+protected:
+	/**
+	 * Load the texture data.
+	 *
+	 * Load the texture data from the source file. This function is expected
+	 * to set the m_width, m_height, m_format and m_data fields.
+	 *
+	 * @return		Whether the texture data was loaded sucessfully.
+	 */
+	virtual bool loadData() = 0;
+protected:
+	uint32_t m_width;			/**< Width of the texture. */
+	uint32_t m_height;			/**< Height of the texture. */
+	PixelFormat m_format;			/**< Format of the texture. */
+	std::unique_ptr<char []> m_buffer;	/**< Buffer containing texture data. */
+};
--- engine/src/engine/loaders/tga_loader.cc
+++ engine/src/engine/loaders/tga_loader.cc
@@ -7,13 +7,12 @@
  *			packed pixel formats).
  */
 
-#include "engine/asset_loader.h"
-#include "engine/texture.h"
+#include "texture_loader.h"
 
 /** TGA texture loader class. */
-class TGALoader : public AssetLoader {
+class TGALoader : public Texture2DLoader {
 public:
-	AssetPtr load() override;
+	bool loadData() override;
 private:
 	/** TGA image file header. */
 	struct Header {
@@ -35,41 +34,38 @@ private:
 IMPLEMENT_ASSET_LOADER(TGALoader, "tga");
 
 /** Load a TGA file.
- * @return		Pointer to loaded asset, null on failure. */
-AssetPtr TGALoader::load() {
+ * @return		Whether the texture data was loaded sucessfully. */
+bool TGALoader::loadData() {
 	Header header;
 	if(!m_data->read(reinterpret_cast<char *>(&header), sizeof(header), 0)) {
 		logError("%s: Failed to read asset data", m_path);
-		return nullptr;
+		return false;
 	}
 
 	/* Only support uncompressed RGB images for now. */
 	if(header.imageType != 2) {
 		logError("%s: Unsupported image format (%u)", m_path, header.imageType);
-		return nullptr;
+		return false;
 	}
 
 	if(header.depth != 24 && header.depth != 32) {
 		logError("%s: Unsupported depth (%u)", m_path, header.depth);
-		return nullptr;
+		return false;
 	}
 
 	/* Determine image properties. */
-	uint32_t width = header.width;
-	uint32_t height = header.height;
-	PixelFormat format = (header.depth == 32) ? PixelFormat::kB8G8R8A8 : PixelFormat::kB8G8R8;
+	m_width = header.width;
+	m_height = header.height;
+	m_format = (header.depth == 32) ? PixelFormat::kB8G8R8A8 : PixelFormat::kB8G8R8;
 
 	/* Read in the data, which is after the ID and colour map. */
-	size_t size = width * height * (header.depth / 8);
+	size_t size = m_width * m_height * (header.depth / 8);
 	uint64_t offset = header.idLength + (header.colourMapLength * (header.colourMapDepth / 8));
-	std::unique_ptr<char []> buf(new char[size]);
-	if(!m_data->read(buf.get(), size, offset)) {
+	m_buffer.reset(new char[size]);
+	if(!m_data->read(m_buffer.get(), size, offset)) {
 		logError("%s: Failed to read asset data", m_path);
-		return nullptr;
+		return false;
 	}
 
-	/* Create the texture, with mipmaps. */
-	Texture2DPtr texture(new Texture2D(width, height, format));
-	texture->update(buf.get());
-	return texture;
+	return true;
 }
