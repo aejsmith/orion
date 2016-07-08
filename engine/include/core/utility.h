e9ed5b1afa2fb61ea7f47d9238134280fd8aff86
Alex Smith <alex@alex-smith.me.uk>
2016-07-08 14:27:18 +0100

    Add ScopeGuard class which calls a function when it is destroyed
    This can be used to call an arbitrary function at the end of a scope,
    useful for doing cleanup when there are many ways out of a scope.
    
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- engine/include/core/utility.h
+++ engine/include/core/utility.h
@@ -29,3 +29,54 @@ template <typename T, size_t N>
 constexpr size_t arraySize(T (&array)[N]) {
     return N;
 }
+
+/** Class to call a function when it is destroyed.
+ * @see                 makeScopeGuard(). */
+template <typename Function>
+class ScopeGuard {
+public:
+    ScopeGuard(Function f) :
+        m_function(std::move(f)),
+        m_active(true)
+    {}
+
+    ~ScopeGuard() {
+        if (m_active)
+            m_function();
+    }
+
+    ScopeGuard(ScopeGuard &&other) :
+        m_function(std::move(other.m_function)),
+        m_active(other.m_active)
+    {
+        other.cancel();
+    }
+
+    /** Cancel the guard (don't call the function). */
+    void cancel() {
+        m_active = false;
+    }
+private:
+    Function m_function;
+    bool m_active;
+
+    ScopeGuard() = delete;
+    ScopeGuard(const ScopeGuard &) = delete;
+    ScopeGuard &operator =(const ScopeGuard &) = delete;
+};
+
+/**
+ * Helper to call a function at the end of a scope.
+ *
+ * The returned object will call the specified function when it is destroyed,
+ * unless cancelled by calling cancel().
+ *
+ * Usage: auto guard = makeScopeGuard([] { action(); });
+ * Calls action when guard goes out of scope.
+ *
+ * @param function      Function to call.
+ */
+template <typename Function>
+ScopeGuard<Function> makeScopeGuard(Function function) {
+    return ScopeGuard<Function>(std::move(function));
+}
