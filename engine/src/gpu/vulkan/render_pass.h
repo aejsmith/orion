9bb2ac199973e80a890b47b58afba4ec9b7e639b
Alex Smith <alex@alex-smith.me.uk>
2016-09-18 12:49:58 +0100

    Vulkan: Add VulkanRenderPassCompatibilityKey
    Vulkan has a number of cases (framebuffers, pipelines) where an object is
    created against a specific render pass object, but can be used with any
    render pass that is compatible with that one. This type encapsulates the
    checking for render pass compatibility, and can be used for caching
    objects with this behaviour.
    
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- engine/src/gpu/vulkan/render_pass.h
+++ engine/src/gpu/vulkan/render_pass.h
@@ -33,6 +33,73 @@ public:
 };
 
 /**
+ * Vulkan render pass compatibility key.
+ *
+ * This key is used to determine whether render passes are compatible with each
+ * other. This is used for framebuffers and pipeline objects, which are created
+ * with a specific render pass but can be used with any that are compatible
+ * with it.
+ *
+ * For now we only have to worry about attachment count and formats because we
+ * don't use subpasses. This will need to be updated if we do in future.
+ */
+struct VulkanRenderPassCompatibilityKey {
+    /** Structure describing an attachment. */
+    struct Attachment {
+        PixelFormat format;             /**< Pixel format of the attachment. */
+        // TODO: Sample count.
+    };
+
+    /** Array of colour attachments. */
+    std::vector<Attachment> colourAttachments;
+
+    /** Depth/stencil attachment. */
+    Attachment depthStencilAttachment;
+
+    /** Initialise the key from a render pass.
+     * @param pass          Pass to initialise from. */
+    explicit VulkanRenderPassCompatibilityKey(const VulkanRenderPass *pass) {
+        const auto &desc = pass->desc();
+
+        colourAttachments.reserve(desc.colourAttachments.size());
+        for (const auto &attachment : desc.colourAttachments) {
+            colourAttachments.emplace_back();
+            colourAttachments.back().format = attachment.format;
+        }
+
+        depthStencilAttachment.format = desc.depthStencilAttachment.format;
+    }
+
+    /** Compare this key with another. */
+    bool operator ==(const VulkanRenderPassCompatibilityKey &other) const {
+        if (colourAttachments.size() != other.colourAttachments.size())
+            return false;
+
+        for (size_t i = 0; i < colourAttachments.size(); i++) {
+            if (colourAttachments[i].format != other.colourAttachments[i].format)
+                return false;
+        }
+
+        if (depthStencilAttachment.format != other.depthStencilAttachment.format)
+            return false;
+
+        return true;
+    }
+
+    /** Get a hash from a render pass compatibility key. */
+    friend size_t hashValue(const VulkanRenderPassCompatibilityKey &key) {
+        size_t hash = hashValue(key.colourAttachments.size());
+
+        for (size_t i = 0; i < key.colourAttachments.size(); i++)
+            hash = hashCombine(hash, key.colourAttachments[i].format);
+
+        hash = hashCombine(hash, key.depthStencilAttachment.format);
+
+        return hash;
+    }
+};
+
+/**
  * Key for a Vulkan framebuffer.
  *
  * This is a key to find framebuffers with given render targets that are
@@ -43,27 +110,27 @@ struct VulkanFramebufferKey {
     /** Render targets. */
     GPURenderTargetDesc targets;
 
+    /** Render pass compatibility key. */
+    VulkanRenderPassCompatibilityKey renderPass;
+
     /** Initialise the key.
      * @param inTargets     Render target descriptor.
      * @param inPass        Pass that the framebuffer should be compatible with. */
     VulkanFramebufferKey(const GPURenderTargetDesc &inTargets, const VulkanRenderPass *inPass) :
-        targets(inTargets)
-    {
-        /* Nothing else to do here for now, since we only support a single
-         * subpass at the moment. This means all that matters is the format
-         * and sample count of the attachments, which is covered by equality of
-         * the render target descriptors. If we add multiple subpass support,
-         * we will need to handle that here. */
-    }
+        targets(inTargets),
+        renderPass(inPass)
+    {}
 
     /** Compare this key with another. */
     bool operator ==(const VulkanFramebufferKey &other) const {
-        return targets == other.targets;
+        return targets == other.targets && renderPass == other.renderPass;
     }
 
     /** Get a hash from a framebuffer key. */
     friend size_t hashValue(const VulkanFramebufferKey &key) {
-        return hashValue(key.targets);
+        size_t hash = hashValue(key.targets);
+        hash = hashCombine(hash, key.renderPass);
+        return hash;
     }
 };
 
