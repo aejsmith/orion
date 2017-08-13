e8363723441fc6cac344e08379ecb63a85763692
Alex Smith <alex@alex-smith.me.uk>
2017-08-13 16:43:21 +0100

    Implement normal mapping
    The new lit_{diffuse,specular}_normal shaders implement this. Requires
    the mesh to have tangents.
    
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- engine/shaders/lit_deferred_frag.glsl
+++ engine/shaders/lit_deferred_frag.glsl
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Alex Smith
+ * Copyright (C) 2015-2017 Alex Smith
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -23,6 +23,11 @@ layout(location = 0) in vec3 vtxPosition;
 layout(location = 1) in vec3 vtxNormal;
 layout(location = 2) in vec2 vtxTexcoord;
 
+#ifdef NORMALMAP
+layout(location = 3) in vec3 vtxTangent;
+layout(location = 4) in vec3 vtxBitangent;
+#endif
+
 layout(location = 0) out vec4 deferredBufferA;
 layout(location = 1) out vec4 deferredBufferB;
 layout(location = 2) out vec4 deferredBufferC;
@@ -34,9 +39,24 @@ void main() {
         vec4 diffuse = vec4(diffuseColour, 1.0);
     #endif
 
-    /* Normal must be re-normalized as the interpolated normal may not be, and
-     * then it must be scaled to be stored in the unorm texture. */
-    deferredBufferA.rgb = (normalize(vtxNormal) + 1.0) / 2.0;
+    #ifdef NORMALMAP
+        vec3 sampledNormal = texture(normalTexture, vtxTexcoord).rgb;
+
+        /* Scale it back to the [-1, 1] range. */
+        sampledNormal = normalize((sampledNormal * 2.0) - 1.0);
+
+        /* Transform from tangent space to world space. */
+        mat3 tangentToWorld = mat3(normalize(vtxTangent),
+                                   normalize(vtxBitangent),
+                                   normalize(vtxNormal));
+        vec3 normal = tangentToWorld * sampledNormal;
+    #else
+        vec3 normal = vtxNormal;
+    #endif
+
+    /* Normal must be re-normalised as the interpolated normal may not be, and
+    /* then scaled to [0, 1] to be stored in the unorm texture. */
+    deferredBufferA.rgb = (normalize(normal) + 1.0) / 2.0;
     deferredBufferA.a = 1.0;
 
     /* Write diffuse colour. */
--- engine/shaders/lit_vtx.glsl
+++ engine/shaders/lit_vtx.glsl
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Alex Smith
+ * Copyright (C) 2015-2017 Alex Smith
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -23,14 +23,34 @@ layout(location = kPositionSemantic) in vec3 attribPosition;
 layout(location = kNormalSemantic) in vec3 attribNormal;
 layout(location = kTexcoordSemantic) in vec2 attribTexcoord;
 
+#ifdef NORMALMAP
+layout(location = kTangentSemantic) in vec4 attribTangent;
+#endif
+
 layout(location = 0) out vec3 vtxPosition;
 layout(location = 1) out vec3 vtxNormal;
 layout(location = 2) out vec2 vtxTexcoord;
 
+#ifdef NORMALMAP
+layout(location = 3) out vec3 vtxTangent;
+layout(location = 4) out vec3 vtxBitangent;
+#endif
+
 void main() {
     vtxPosition = vec3(entity.transform * vec4(attribPosition, 1.0));
-    vtxNormal = vec3(entity.transform * vec4(attribNormal, 0.0));
+    vtxNormal   = vec3(entity.transform * vec4(attribNormal, 0.0));
     vtxTexcoord = attribTexcoord;
+
+    #ifdef NORMALMAP
+        vec3 tangent = attribTangent.xyz;
+
+        /* Calculate tangent and bitangent in world space so we can transform
+         * directly from tangent space to world space in the fragment shader.
+         * Bitangent is reconstructed from normal and tangent, handedness is
+         * stored in W component of the attribute. */
+        vtxTangent   = vec3(entity.transform * vec4(tangent, 0.0));
+        vtxBitangent = cross(vtxNormal, vtxTangent) * attribTangent.w;
+    #endif
 
     gl_Position = view.viewProjection * entity.transform * vec4(attribPosition, 1.0);
 }
