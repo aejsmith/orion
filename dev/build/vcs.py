3e4fbe0d7fe2ceb17c38abb1fb3668ec131dbcdd
Alex Smith <alex@alex-smith.me.uk>
2017-08-27 18:39:34 +0100

    Fix build without git in PATH
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- dev/build/vcs.py
+++ dev/build/vcs.py
@@ -1,11 +1,14 @@
 # Obtain the revision number from the Git repository.
 def revision_id():
-    from subprocess import Popen, PIPE
-    git = Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout = PIPE, stderr = PIPE)
-    revision = git.communicate()[0].strip()
-    if git.returncode != 0:
+    try:
+        from subprocess import Popen, PIPE
+        git = Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout = PIPE, stderr = PIPE)
+        revision = git.communicate()[0].strip()
+        if git.returncode != 0:
+            return None
+        return revision
+    except:
         return None
-    return revision
 
 # Check whether submodules are up to date.
 def check_submodules():
